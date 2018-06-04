18c4e42507220bd5fd25201af28ae710b46d4bf1
Alexandre Henrique <alexandrehcdc@gmail.com>
2018-06-04 13:28:33 -0300

    removed package-lock.json

--- Routes/index.js
+++ Routes/index.js
@@ -2,14 +2,15 @@
 
 const express = require('express')
 const router = new express.Router()
+const path = require('path')
 const {readFile} = require('fs')
 
-router.route('/')
-    .get((req, res, next) => {
-        readFile(__dirname + '/../bases_samu_al.json','utf8', (err, data) => {
-            if (err) throw err
-            res.send(data)
-        })
+router.route('/locations')
+  .get((req, res, next) => {
+    readFile(path.join(__dirname, '/../bases_samu_al.json'), 'utf8', (err, data) => {
+      if (err) throw err
+        res.send(data)
     })
+  })
 
 module.exports = router
